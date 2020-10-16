56b5b458c271f2796d3c5d94b47ba795e9e0f297
sandeep194920 <sandeepamaranath@gmail.com>
2020-10-15 22:47:02 -0400

    created Card and index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Card } from "./components";
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
+    <div>
+      <Card />
     </div>
   );
 }
