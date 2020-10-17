5ff17b7f5b6e875b8efc9e0fd4054922cfb1a296
sandeep194920 <sandeepamaranath@gmail.com>
2020-10-17 14:35:39 -0400

    importing images into App folder since the image paths in data.json dont work on gh-pages

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React from "react";
 import "./App.scss";
 import { Card } from "./components/Card";
 import movies from "./data";
+import joker from "../src/images/joker.jpg";
+import despicable from "../src/images/despicable-me.jpg";
+import thePrestige from "../src/images/the-prestige.jpg";
+
 function App() {
   return (
     <main>
-      {movies.map((movie) => (
-        <Card classes="mr" key={movie.id}>
-          <Card.Image src={movie.image} alt={movie.title} />
-          <Card.Body>
-            <Card.Title>{movie.title}</Card.Title>
-            <Card.Text>{movie.desc} </Card.Text>
-            <Card.Button>{movie.ctaText} </Card.Button>
-          </Card.Body>
-        </Card>
-      ))}
+      {movies.map((movie) => {
+        let img = joker;
+        if (movie.title === "The Prestige") {
+          img = thePrestige;
+        } else if (movie.title === "Despicable Me") {
+          img = despicable;
+        }
+        return (
+          <Card classes="mr" key={movie.id}>
+            <Card.Image src={img} alt={movie.title} />
+            <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>{movie.desc} </Card.Text>
+              <Card.Button>{movie.ctaText} </Card.Button>
+            </Card.Body>
+          </Card>
+        );
+      })}
     </main>
   );
 }
